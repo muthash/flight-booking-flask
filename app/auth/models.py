ea235769d5b028b8345092ec0559f6ad0bb908bc
muthash <kathingo92@gmail.com>
2019-06-26 23:17:46 +0300

    Update users model to include confirmation email

--- app/auth/models.py
+++ app/auth/models.py
@@ -14,21 +14,23 @@ class User(BaseModel):
     email = db.Column(db.String(120), nullable=False, unique=True)
     name = db.Column(db.String(256), nullable=False)
     password = db.Column(db.String(256), nullable=False)
-    passport = db.Column(db.String(256))
+    passport = db.Column(db.String(256), default="1")
     registered_on = db.Column(db.DateTime, default=db.func.current_timestamp())
     is_admin = db.Column(db.Boolean, nullable=False, default=False)
-    bookings = db.relationship('Booking', backref='user', 
+    confirmed = db.Column(db.Boolean, nullable=False, default=False)
+    confirmed_on = db.Column(db.DateTime, nullable=True)
+    bookings = db.relationship('Booking', backref='user',
                                order_by='Booking.id',
                                cascade="all, delete-orphan", lazy=True)
 
-    def __init__(self, email, name, password, passport, is_admin=False):
+    def __init__(self, email, name, password, is_admin=False, confirmed=False):
         """Initialize the user with the user details"""
         self.email = email
         self.name = name
         self.password = Bcrypt().generate_password_hash(password).decode()
-        self.passport = passport
         self.registered_on = datetime.datetime.now()
         self.is_admin = is_admin
+        self.confirmed = confirmed
 
     def password_is_valid(self, password):
         """Check the password against its hash"""
