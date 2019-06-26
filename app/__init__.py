8bf5622e6fae32aeae80768e0308e1074d2c3ffb
muthash <kathingo92@gmail.com>
2019-06-26 23:14:14 +0300

    Add default 422 and 400 error handler

--- app/__init__.py
+++ app/__init__.py
@@ -21,4 +21,18 @@ def create_app(config_name):
     db.init_app(app)
     jwt.init_app(app)
 
+    from app.auth.views import auth
+
+    @app.errorhandler(422)
+    @app.errorhandler(400)
+    def handle_error(err):
+        headers = err.data.get("headers", None)
+        messages = err.data.get("messages", ["Invalid request."])
+        if headers:
+            return jsonify({"errors": messages}), err.code, headers
+        else:
+            return jsonify({"errors": messages}), err.code
+
+    app.register_blueprint(auth)
+
     return app
