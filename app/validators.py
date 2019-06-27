475ddee7652b5318f2fdb01b3609dec44af78ad1
muthash <kathingo92@gmail.com>
2019-06-27 17:19:08 +0300

    Normalize email for effective data query

--- app/validators.py
+++ app/validators.py
@@ -12,6 +12,11 @@ user_args = {
     "name": fields.Str(required=True, validate=validate.Length(min=2)),
     }
 
+login_args = {
+    "email": fields.Str(required=True, validate=validate.Email()),
+    "password": fields.Str(required=True),
+    }
+
 
 def check_email(email):
     """Check for a valid email address and
@@ -25,6 +30,7 @@ def check_email(email):
         response = {'message': str(error)}
         return jsonify(response), 400
 
+
 def check_name(name):
     regex = re.compile('^[a-zA-Z]{3,}$')
     res = re.match(regex, str(name))
@@ -36,9 +42,18 @@ def check_name(name):
                                "please try again but with a different name."}
         return jsonify(response), 400
 
+
 def check_password(password):
     if not re.match(r'[a-zA-Z_]+[A-Za-z0-9@#!$%^&+=]{8,}', str(password)):
         response = {'message': 'Password should contain at least eight ' +
-                           'characters with at least one digit, one ' +
-                           'uppercase letter and one lowercase letter.'}
+                    'characters with at least one digit, one ' +
+                    'uppercase letter and one lowercase letter.'}
         return jsonify(response), 400
+
+
+def normalize_email(email):
+    """Lowercase the domain part of the email"""
+    email_part = email.split('@')
+    domain = email_part[1].lower()
+    email = email_part[0]+'@'+domain
+    return email
