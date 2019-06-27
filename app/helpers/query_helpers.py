3e6c9f95fe1447df6031b55928ec29c1d4b5b276
muthash <kathingo92@gmail.com>
2019-06-27 17:18:26 +0300

    Add token generation function

--- app/helpers/query_helpers.py
+++ app/helpers/query_helpers.py
@@ -1,4 +1,23 @@
+import datetime
+
+from flask import jsonify
+from flask_jwt_extended import create_access_token
+
 from app.auth.models import User
+
+
+def generate_token(message, user, expires=datetime.timedelta(hours=1)):
+    """Return access token and response to user"""
+    response = {'message': message,
+                'access_token': create_access_token(
+                        identity=user.id, expires_delta=expires)}
+    return jsonify(response), 200
+
+
+def generate_response(message, status):
+    """Return application/json object"""
+    response = {'message': message}
+    return jsonify(response), status
 
 
 def search_email(email):
@@ -8,3 +27,4 @@ def search_email(email):
 def save_user(email, name, password):
     user = User(email, name, password)
     user.save()
+
