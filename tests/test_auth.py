39111251e4807fa2bc9ebbd00b623c55ffa1a5a7
muthash <kathingo92@gmail.com>
2019-06-27 17:17:43 +0300

    Test login user works as expected

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -11,17 +11,20 @@ class TestRegisterUser(BaseTestCase):
         res = self.client.post('/api/register', headers=self.header,
                                data=json.dumps(self.reg_data))
         result = json.loads(res.data.decode())
-        self.assertEqual(result['message'], "Account created successfully, " +
-                         "A confirmation email has been sent.")
+        self.assertEqual(result['message'], "Account created successfully")
         self.assertEqual(res.status_code, 201)
 
 
-        # def test_registration(self):
-        # """Test user registration works correcty."""
-        # res = self.client().post('/auth/register', data=self.user_data)
-        # # get the results returned in json format
-        # result = json.loads(res.data.decode())
-        # # assert that the request contains a success message and a 201 status code
-        # self.assertEqual(result['message'], "You registered successfully.")
-        # self.assertEqual(res.status_code, 201)
+class TestLoginUser(BaseTestCase):
+    """Test for Login User endpoint"""
 
+    def test_user_login(self):
+        """Test registered user can login."""
+        self.client.post('/api/register', headers=self.header,
+                         data=json.dumps(self.reg_data))
+        res = self.client.post('/api/login', headers=self.header,
+                               data=json.dumps(self.reg_data))
+        result = json.loads(res.data.decode())
+        self.assertEqual(result['message'], "Login successfull")
+        self.assertTrue(result['access_token'])
+        self.assertEqual(res.status_code, 200)
