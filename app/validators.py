614599b3be44dc743223bb56d960332589146ae0
muthash <kathingo92@gmail.com>
2019-06-27 03:14:20 +0300

    Add email, name and password validators

--- app/validators.py
+++ app/validators.py
@@ -3,13 +3,13 @@ import re
 from flask import jsonify
 from email_validator import validate_email, EmailNotValidError
 from webargs import fields, validate
+from usernames import is_safe_username
 
 
 user_args = {
     "email": fields.Str(required=True, validate=validate.Email()),
     "password": fields.Str(required=True, validate=validate.Length(min=8)),
-    "firstname": fields.Str(required=True, validate=validate.Length(min=2)),
-    "othernames": fields.Str(required=True, validate=validate.Length(min=2)),
+    "name": fields.Str(required=True, validate=validate.Length(min=2)),
     }
 
 
@@ -23,4 +23,22 @@ def check_email(email):
         return email
     except EmailNotValidError as error:
         response = {'message': str(error)}
+        return jsonify(response), 400
+
+def check_name(name):
+    regex = re.compile('^[a-zA-Z]{3,}$')
+    res = re.match(regex, str(name))
+    if not res:
+        response = {'message': "The name should only contain letters."}
+        return jsonify(response), 400
+    if not is_safe_username(name):
+        response = {'message': "The name you provided is not allowed, " +
+                               "please try again but with a different name."}
+        return jsonify(response), 400
+
+def check_password(password):
+    if not re.match(r'[a-zA-Z_]+[A-Za-z0-9@#!$%^&+=]{8,}', str(password)):
+        response = {'message': 'Password should contain at least eight ' +
+                           'characters with at least one digit, one ' +
+                           'uppercase letter and one lowercase letter.'}
         return jsonify(response), 400
